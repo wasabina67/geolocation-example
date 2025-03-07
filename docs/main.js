c0002e61e570170e6afd77f8b2a935cd577c4632
wasabina67 <wasabina67@gmail.com>
2025-03-07 12:54:54 +0900

    Update docs/main.js

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,10 @@
-function successCallback(position) {}
+function successCallback(position) {
+  document.getElementById('location').innerHTML = 'Supported';
+}
 
-function errorCallback(error) {}
+function errorCallback(error) {
+  document.getElementById('location').textContent = 'Error';
+}
 
 if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
