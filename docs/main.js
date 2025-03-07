5e255c285676cd7fa30d69f649ccecad3054e6d6
wasabina67 <wasabina67@gmail.com>
2025-03-07 12:50:28 +0900

    Update docs/main.js

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,9 @@
+function successCallback() {}
 
+function errorCallback() {}
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+} else {
+  document.getElementById('location').textContent = 'Not supported';
+}
